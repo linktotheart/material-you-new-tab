8a65c9adfe630f74bff331ad73b03eaac246afe7
Pulkit <pulkit.singh@journeyfront.com>
2024-09-07 23:56:09 +0530

    init -- material you

---
+++ src/components/ClockAndWeatherWidget.vue
@@ -0,0 +1,15 @@
+<template>
+  <MaterialClock />
+</template>
+
+<script>
+import MaterialClock from './MaterialClock.vue'
+export default {
+	components: { MaterialClock },
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './src/**/*.html',
+    './src/**/*.vue',
+    './src/**/*.{js,jsx,ts,tsx}',
+    './src/**/*.{css, scss, sass}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
